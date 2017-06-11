c4d1852580402cd7b1cfa862df6108111447bd41
md-farhan-memon <md.farhanmemon@gmail.com>
2017-06-12 02:00:23 +0530

    rails update, readme and turbolink reload fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,61 +11,62 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
 
-$(function(){
-  $(document.body).on('click', '#run', function(e){
-    $('.clear').fadeIn();
-    // $('.clear').removeAttr('display');
-    var load = true;
-    var count = 1;
+document.addEventListener("turbolinks:load", function() {
+  $(function(){
+    $(document.body).on('click', '#run', function(e){
+      $('.clear').fadeIn();
+      // $('.clear').removeAttr('display');
+      var load = true;
+      var count = 1;
 
-    var cells = [];
-    $('.active').each(function(){
-      var col = parseInt($(this).attr('col'));
-      var row = parseInt($(this).attr('row'));
-      cells.push([row,col]);
+      var cells = [];
+      $('.active').each(function(){
+        var col = parseInt($(this).attr('col'));
+        var row = parseInt($(this).attr('row'));
+        cells.push([row,col]);
+      });
+
+      (function loop() {
+        e.preventDefault();
+
+        if (window.clear) {
+          window.clear = false;
+          return false;
+        }
+
+        $.post('/start', {load: load, cells: cells});
+        $('#run').addClass('disabled').text(count);
+        count ++;
+        setTimeout(function(){
+          load = false;
+          loop();
+       }, 800);
+      }());
     });
+  });
 
-    (function loop() {
-      e.preventDefault();
-
-      if (window.clear) {
-        window.clear = false;
-        return false;
+  $(function(){
+    $(document.body).on('click', '.cell', function(){
+      $('.clear').fadeIn();
+      $('.clear').removeAttr('display');
+      $('#run').removeClass('disabled');
+      if(!$(this).hasClass("active")) {
+        $(this).addClass("active");
+      } else {
+        $(this).removeClass("active");
       }
-
-      $.post('/start', {load: load, cells: cells});
-      $('#run').addClass('disabled').text(count);
-      count ++;
-      setTimeout(function(){
-        load = false;
-        loop();
-     }, 700); 
-    }());
+    });
   });
-});
 
-$(function(){
-  $(document.body).on('click', '.cell', function(){
-    $('.clear').fadeIn();
-    $('.clear').removeAttr('display');
-    $('#run').removeClass('disabled');
-    if(!$(this).hasClass("active")) {
-      $(this).addClass("active");
-    } else {
-      $(this).removeClass("active");
-    }
+  $(function(){
+    $(document.body).on('click', '.clear', function(){
+      $(this).fadeOut();
+      window.clear = true;
+      $.post('/clear', {});
+    });
   });
-});
-
-$(function(){
-  $(document.body).on('click', '.clear', function(){
-    $(this).fadeOut();
-    window.clear = true;
-    $.post('/clear', {});
-  });
-});
+})
